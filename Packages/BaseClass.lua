162be8d4b26ea4c66a188365424e5a7b9ef4f013
Dejay Clayton <dejay@dejayclayton.com>
2012-03-27 23:30:59 -0400

    Completely refactored 'BaseClass' into a star-spangled new version

--- Packages/BaseClass.lua
+++ Packages/BaseClass.lua
@@ -9,172 +9,304 @@
 
 local BaseLua = require( "BaseLua" )
 local ClassHelper = require( BaseLua.package.ClassHelper )
+local TableHelper = require( BaseLua.package.TableHelper )
 
-local CLASS = { className = ClassHelper.getPackagePath( ... ) }
+local CLASS_NAME = ClassHelper.getPackagePath( ... )
+local OBJECT_LABEL_ID_OFFSET = string.len( "table " ) + 2
 
-local function findMetamethod( target, index, ignoreFn )
-    local toInvoke
+local METAMETHOD_NAMES = {
+    '__mul', '__pow', '__div', '__mod', '__add', '__sub', '__unm',
+    '__le', '__lt',
+    '__concat', '__tostring',
+    '__call', '__index', '__newindex',
+}
 
-    -- First, check the class instance properties for the specified
-    -- metamethod, which will not propagate to super classes.
-    local class = TableHelper.selectByNestedIndex(
-        target, "class", "instanceProperties" )
+local ERR_MSG_FN_CREATE_CLASS_INSTANCE_ARG_BASE_CLASS_INVALID =
+    "Function 'createClassInstance' in " .. CLASS_NAME ..
+    " was supplied an invalid value for parameter 'baseClass'"
+local ERR_MSG_FN_CREATE_CLASS_INSTANCE_METATABLE_ARG_BASE_CLASS_INVALID =
+    "Function 'createClassInstanceMetatable' in " .. CLASS_NAME ..
+    " was supplied an invalid value for parameter 'baseClass'"
+local ERR_MSG_FN_CREATE_CLASS_INSTANCE_METATABLE_ARG_SELF_INVALID =
+    "Function 'createClassInstanceMetatable' in " .. CLASS_NAME ..
+    " was supplied an invalid value for parameter 'self'"
+local ERR_MSG_FN_COPY_CLASS_METATABLE_ARG_BASE_CLASS_INVALID =
+    "Function 'copyClassMetatable' in " .. CLASS_NAME ..
+    " was supplied an invalid value for parameter 'baseClass'"
+local ERR_MSG_FN_COPY_CLASS_METATABLE_ARG_SELF_INVALID =
+    "Function 'copyClassMetatable' in " .. CLASS_NAME ..
+    " was supplied an invalid value for parameter 'self'"
+local ERR_MSG_FN_CREATE_SUBCLASS_ARG_BASE_CLASS_INVALID =
+    "Function 'createSubclass' in " .. CLASS_NAME ..
+    " was supplied an invalid value for parameter 'baseClass'"
+local ERR_MSG_FN_EXTEND_ARG_BASE_CLASS_INVALID =
+    "Function 'extend' in " .. CLASS_NAME ..
+    " was supplied an invalid value for parameter 'baseClass'"
+local ERR_MSG_FN_EXTEND_ARG_SELF_INVALID =
+    "Function 'extend' in " .. CLASS_NAME ..
+    " was supplied an invalid value for parameter 'self'"
+local ERR_MSG_FN_NEW_ARG_BASE_CLASS_INVALID =
+    "Function 'new' in " .. CLASS_NAME ..
+    " was supplied an invalid value for parameter 'baseClass'"
 
-    if ( class ~= nil) then
-        toInvoke = rawget( class, index )
-        if ( toInvoke ~= nil and toInvoke ~= ignoreFn ) then
-            return toInvoke
+-- An array of shared class instance metatables that instances of a particular
+-- class can share to conserve memory.
+local classInstanceMetatables = setmetatable( { }, { __mode = "kv" } )
+
+-- An array of shared class instance properties that instances of a particular
+-- class can share to conserve memory.
+local classInstanceProperties = setmetatable( { }, { __mode = "kv" } )
+
+-- Forward declaration of methods that need it.
+local createClass, createClassInstance, createSubclass 
+
+local function onProxyTableError( target, propertyName, value )
+    error(
+        "Attempt to set protected class properties with index '" ..
+        tostring( propertyName ) .. "'", 3 )
+end
+
+local function ancestors( class, includeSelf )
+    -- Keep track of encountered objects to prevent any infinite recursion.
+    local encountered = { }
+
+    -- Only store weak references in the tracking array, otherwise objects
+    -- might be held up from garbage collection.
+    setmetatable( encountered, { __mode = "k" } )
+
+    -- If class is specified to be included in the ancestry chain, create a
+    -- new class structure with the specified class as the starting node.
+    if ( includeSelf ) then class = { class = { super = class } } end
+
+    return function()
+        class = TableHelper.getByIndex( class, "class", "super" )
+
+        if ( class == nil ) then
+            -- Release the tracking array.
+            encountered = nil
+            return
         end
-    end
 
-    -- Next, check the class raw table properties for the specified
-    -- metamethod, which will not propagate to super classes.
-    toInvoke = rawget( target, index )
-    if ( toInvoke ~= nil and toInvoke ~= ignoreFn ) then
-        return toInvoke
-    end
-
-    -- Next, check the class metatable properties for the specified
-    -- metamethod, which will not propagate to super classes.
-    local metatable = getmetatable( target )
-
-    if ( metatable ~= target and metatable ~= nil ) then
-        toInvoke = rawget( metatable, index )
-        if ( toInvoke ~= nil and toInvoke ~= ignoreFn ) then
-            return toInvoke
+        if ( not encountered[ class ] ) then
+            encountered[ class ] = true
+            return class
         end
-    end
 
-    -- Next, check the class table properties for the specified metamethod,
-    -- which MAY propagate to super classes, depending on how "__index" is
-    -- defined for this table.  Only do this for metamethod lookups which are
-    -- not "__index", as that would lead to infinite recursion.
-    if ( metatable == nil or metatable.__index ~= ignoreFn ) then
-        toInvoke = target[ index ]
-        if ( toInvoke ~= nil and toInvoke ~= ignoreFn ) then
-            return toInvoke
-        end
-    end
-
-    -- Next, check the metatable table properties for the specified
-    -- metamethod, which MAY propagate to super classes, depending on whether
-    -- the metatable has its own metatable defined with an "__index"
-    -- metamethod.
-    if ( metatable ~= target and metatable ~= nil ) then
-        toInvoke = metatable[ index ]
-        if ( toInvoke ~= nil and toInvoke ~= ignoreFn ) then
-            return toInvoke
-        end
+        -- Release the tracking array.
+        encountered = nil
+        return
     end
 end
 
-local function createMetamethodLookup( target, metamethodName )
-    return function( ... )
-        local metatable = getmetatable( target ) or {}
-        local ignoreFn = metatable[ metamethodName ]
+local function getClassName( class )
+    return ( TableHelper.getByIndex( class, "class", "name" ) )
+end
 
-        -- First, check the current table for the specified metamethod.
-        local toInvoke = findMetamethod( target, metamethodName, ignoreFn )
-        if ( toInvoke ~= nil ) then return toInvoke( ... ) end
+local function getConstructor( class )
+    return (
+        TableHelper.getByIndex( class, "constructor" ) or
+        TableHelper.getByIndex( class, "class", "constructor" ) )
+end
 
-        -- Next, check the super class for the specified metamethod.
-        local super = TableHelper.selectByNestedIndex(
-            target, "class", "super" )
+local function getObjectId( self )
+    return TableHelper.getByIndex( self, "object", "id" )
+end
 
-        if ( super ~= nil) then
-            toInvoke = findMetamethod( super, metamethodName, ignoreFn )
-            if ( toInvoke ~= nil ) then return toInvoke ( ... ) end
+local function copyClassMetatable( baseClass, self )
+    assert( type( baseClass ) == "table",
+        ERR_MSG_FN_COPY_CLASS_METATABLE_ARG_BASE_CLASS_INVALID )
+
+    assert( type( self ) == "table",
+        ERR_MSG_FN_COPY_CLASS_METATABLE_ARG_SELF_INVALID )
+
+    local metatable = getmetatable( self )
+
+    if ( metatable == nil ) then
+        metatable = self
+        setmetatable( self, metatable )
+    end
+
+    local baseMetatable = getmetatable( baseClass )
+    if ( baseMetatable ) then
+        -- Make all non-present metamethods in the class metatable point to
+        -- their counterparts in the base class metatable.
+        for _, metamethodName in ipairs( METAMETHOD_NAMES ) do
+            if ( metatable[ metamethodName ] == nil ) then
+                metatable[ metamethodName ] = baseMetatable[ metamethodName ]
+            end
         end
     end
+
+    -- Update the metatable to point to the base class.
+    metatable.__index = baseClass
 end
 
-local function initializeMetamethods( target )
-    local metatable = getmetatable( target ) or {}
+local function createClassInstanceMetatable( baseClass, self )
+    assert( type( baseClass ) == "table",
+        ERR_MSG_FN_CREATE_CLASS_INSTANCE_METATABLE_ARG_BASE_CLASS_INVALID )
 
-    for _, metamethodName in ipairs( metamethodNames )
-    do
-        metatable[ metamethodName ] =
-            createMetamethodLookup( target, metamethodName )
-    end
-    return metatable
-end
+    assert( type( self ) == "table",
+        ERR_MSG_FN_CREATE_CLASS_INSTANCE_METATABLE_ARG_SELF_INVALID )
 
-function CLASS:cast( object )
-    object = object or {}
-    setmetatable( object, getmetatable( self.class ))
+    local metatable = classInstanceMetatables[ baseClass ]
 
-    return object
-end
+    if ( metatable == nil ) then
+        metatable = { }
+        setmetatable( self, metatable )
 
-function CLASS:cloneMetatable( source )
-    local sourceMetatable = getmetatable( source )
-    local targetMetatable = {}
-
-    for index, value in pairs( sourceMetatable ) do
-        targetMetatable[ index ] = value
-    end
-
-    return setmetatable( self, targetMetatable )
-end
-
-function CLASS:copyMetatable( source )
-    local sourceMetatable = getmetatable( source )
-    local targetMetatable = getmetatable( self )
-
-    if ( sourceMetatable ~= targetMetatable )
-    then
-        for index, value in pairs( sourceMetatable ) do
-            targetMetatable[ index ] = value
+        local baseMetatable = getmetatable( baseClass )
+        if ( baseMetatable ) then
+            -- Make all metamethods in the shared metatable point to their
+            -- counterparts in the base class metatable.
+            for _, metamethodName in ipairs( METAMETHOD_NAMES ) do
+                metatable[ metamethodName ] = baseMetatable[ metamethodName ]
+            end
         end
+
+        classInstanceMetatables[ baseClass ] = metatable
+
+        -- Update the metatable to point to the base class.
+        metatable.__index = baseClass
     end
+end
+
+local function construct( class, ... )
+    -- Search class and ancestors for a constructor.
+    for ancestor in ancestors( class, true ) do
+        constructor = getConstructor( class )
+        if ( constructor ) then return constructor ( class, ... ) end
+    end
+
+    return class
+end
+
+local function new( baseClass, ... )
+    assert( type( baseClass ) == "table",
+        ERR_MSG_FN_NEW_ARG_BASE_CLASS_INVALID )
+
+    local self = createClassInstance( baseClass )
+    createClassInstanceMetatable( baseClass, self )
+
+    local objectProperties = { }
+    self.object = objectProperties
+
+    objectProperties.id = string.sub(
+        tostring( objectProperties ), OBJECT_LABEL_ID_OFFSET )
+
+    return construct( self, ... )
+end
+
+local function extend( baseClass, self, name )
+    assert( type( baseClass ) == "table",
+        ERR_MSG_FN_EXTEND_ARG_BASE_CLASS_INVALID )
+
+    assert( self == nil or type( self ) == "table",
+        ERR_MSG_FN_EXTEND_ARG_SELF_INVALID )
+
+    local self = createSubclass( baseClass, self, name )
+    copyClassMetatable( baseClass, self )
+
+    local objectProperties = { }
+    self.object = objectProperties
+
+    objectProperties.id = string.sub(
+        tostring( objectProperties ), OBJECT_LABEL_ID_OFFSET )
 
     return self
 end
 
-function CLASS:extend( object )
-    object = object or {}
+local function __tostring( self )
+    local className = getClassName( self ) or CLASS_NAME
+    local id = getObjectId( self )
 
-    --[[
-        Create a class-specific metatable that copies the metatable of the
-        parent class being extended, and sets the fallback prototype chain to
-        index the parent class.
-    --]]
-    object.super = self
-    self.cloneMetatable( object, object.super )
-    getmetatable( object ).__index = object.super
-
-    --[[
-        Create a prototype metatable to be shared by all future child
-        instances of this class, so that the fallback index searches this
-        class before searching the parent class.  This single metatable
-        is shared by all instances of future child instances in order to
-        avoid a per-object metatable and their associated memory overhead.
-    --]]
-    object.class = {}
-    self.cloneMetatable( object.class, object )
-    getmetatable( object.class ).__index = object
-
-    return object
+    if ( id ) then
+        return className .. ": " .. id
+    else
+        return className
+    end
 end
 
---[[
-    'Class:new' is the same as 'Class:cast', except that a new object instance
-    is always created.
---]]
-function CLASS:new()
-    return self:cast( {} )
+createClass = function( name )
+    local self = { }
+    self.class = TableHelper.createReadProxyTable( {
+        ancestors = function()
+            return ancestors( self, false )
+        end,
+        constructor = TableHelper.getByIndex( class, "constructor" ),
+        extend = function( ... )
+            return extend( self, ... )
+        end,
+        name = name,
+        new = function( ... )
+            return new( self, ... )
+        end,
+        super = nil,
+        }, onProxyTableError )
+
+    setmetatable( self, self )
+    return self
 end
 
-function CLASS:__tostring()
-    if ( not self ) then return "undefined" end
-    if ( not self.className ) then return self end
-    if ( not self.super ) then return self.className end
-    if ( not self.super.className ) then return self.className end
+createClassInstance = function( baseClass )
+    assert( type( baseClass ) == "table",
+        ERR_MSG_FN_CREATE_CLASS_INSTANCE_ARG_BASE_CLASS_INVALID )
 
-    return self.className .. " (" .. self.super.className .. ")"
+    local classProperties = classInstanceProperties[ baseClass ]
+
+    if ( classProperties == nil ) then
+        classProperties = TableHelper.createReadProxyTable( {
+            ancestors = function()
+                return ancestors( baseClass, true )
+            end,
+            constructor = TableHelper.getByIndex( class, "constructor" ),
+            extend = function( ... )
+                return extend( baseClass, ... )
+            end,
+            name = getClassName( baseClass ),
+            new = function( ... )
+                return new( baseClass, ... )
+            end,
+            super = baseClass,
+            }, onProxyTableError )
+
+        classInstanceProperties[ baseClass ] = classProperties
+    end
+
+    return { class = classProperties }
 end
 
-setmetatable( CLASS, {
-    __tostring = CLASS.__tostring })
+createSubclass = function( baseClass, self, name )
+    assert( type( baseClass ) == "table",
+        ERR_MSG_FN_CREATE_SUBCLASS_ARG_BASE_CLASS_INVALID )
 
+    if ( baseClass == self ) then return baseClass end
+
+    self = self or { }
+    self.class = TableHelper.createReadProxyTable( {
+        ancestors = function()
+            return ancestors( self, false )
+        end,
+        constructor = function()
+            return getConstructor( self )
+        end,
+        extend = function( ... )
+            return extend( self, ... )
+        end,
+        name = name,
+        new = function( ... )
+            return new( self, ... )
+        end,
+        super = baseClass,
+        }, onProxyTableError )
+
+    return self
+    
+end
+
+local CLASS = createClass( CLASS_NAME )
+
+CLASS.__call = new
+CLASS.__tostring = __tostring
+
+CLASS = construct( CLASS )
 return CLASS
